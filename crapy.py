f89f219a7349a1b81ec9b6d14d8d74138d7ac250
alip putra <asalip.putra@gmail.com>
2023-11-30 18:12:13 +0700

    fix driver get url

--- crapy.py
+++ crapy.py
@@ -3,13 +3,14 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
 class Crapy:
-  def __init__(self):
+  def __init__(self, url):
     options = Options()
     options.add_argument('--headless')
     self.driver = webdriver.Firefox(options=options)
+    self.driver.get(url)
 
-  def get_data(self, url):
-    soup = BeautifulSoup(url.page_source, 'html.parser')
+  def get_data(self):
+    soup = BeautifulSoup(self.driver.page_source, 'html.parser')
     self.driver.close()
     title = soup.find('div', id='zeus-header')
     return title.prettify()
