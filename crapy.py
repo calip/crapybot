629374efbbec0641550d98aceae605a1d7e6ce70
alip putra <asalip.putra@gmail.com>
2023-12-05 16:30:34 +0700

    extract domain from url

--- crapy.py
+++ crapy.py
@@ -1,17 +1,28 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+import re
 
 class Crapy:
   def __init__(self, url):
+    self.driver = self.init_driver()
+    self.driver.get(url)
+    self.domain = self.get_domain(url)
+
+  def init_driver(self):
     options = Options()
     options.add_argument('--headless')
-    self.driver = webdriver.Firefox(options=options)
-    self.driver.get(url)
+    driver = webdriver.Firefox(options=options)
+    return driver
+
+  def get_domain(self, url):
+    pattern = r"(?P<domain>[\w\-]+\.+[\w\-]+)"
+    match = re.search(pattern, url)
+    domain = match.group("domain")
+    return domain
 
   def get_data(self):
     soup = BeautifulSoup(self.driver.page_source, 'html.parser')
     self.driver.close()
     title = soup.find('div', id='zeus-header')
     return title.prettify()
-
